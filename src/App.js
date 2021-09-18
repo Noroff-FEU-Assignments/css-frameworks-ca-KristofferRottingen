50eceefd3d65d53f78008bfe61c465cb2d3ff610
KristofferRottingen <kriroe06@gmail.com>
2021-09-18 19:02:26 +0200

    fiksing router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
 import "./sass/style.scss";
 import Home from "./components/Home";
+import News from "./components/News";
+import Conatct from "./components/Contact";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 function App() {
-  return <Home />;
+  return <Router>
+    <div>
+      <Navbar bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">The YAY Company</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <div className="nav-links">
+                <NavLink className="active" exact to="/">Home</NavLink>
+                <NavLink to="/news">News</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+              </div>
+              <Form inline className="form-search">
+                <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+                <Button className="nav-button" variant="outline-success">Go</Button>
+              </Form>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <hr />
+
+      {/*
+      A <Switch> looks through all its children <Route>
+      elements and renders the first one whose path
+      matches the current URL. Use a <Switch> any time
+      you have multiple routes, but you want only one
+      of them to render at a time
+    */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/news">
+          <News />
+        </Route>
+        <Route path="/contact">
+          <Conatct />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 
 }
 
